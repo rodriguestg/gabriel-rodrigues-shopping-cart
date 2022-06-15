77612bcdf07970e58a7c88a39f48e2d7041a084b
rodriguestg <grs.ibr@gmail.com>
2022-06-15 20:10:32 -0300

    Subindo requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,24 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aquii
-  fail('Teste vazio');
+  it ('Ser uma função', async () => {
+    const fun = await fetchItem;
+    expect(typeof fun).toBe('function');
+  });
+  it ('Chamar o fetchItem quando passado o argumento MLB1615760527', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it ('Utilizar o endpoint, quando chamar a função com o argumento MLB1615760527', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it ('Retornar a mesma estrutura de dados do item, ao chamar a função com argumento MLB1615760527', async () => {
+    const mockResult = await fetchItem('MLB1615760527');
+    expect(mockResult).toStrictEqual(item);
+  });
+  it ('Retornar um erro com a mensagem "You must provide an url", ao chamar a função sem nenhum argumento', async () => {
+    const testError = await fetchItem();
+    expect(testError).toEqual(new Error('You must provide an url'));
+  });
 });
